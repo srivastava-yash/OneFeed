a0351fff78f05c6fdcb3ea9fc425a15bcaef8e2f
srivastava-yash <yash.184040@gmail.com>
2024-04-02 00:31:06 -0400

    landing page nav setup

---
+++ frontend/src/app/components/SignupBtn.js
@@ -0,0 +1,10 @@
+
+export default function SignupBtn() {
+    return (
+        <div className='pt-1.5'>
+            <button className='bg-white text-orange-400 h-10 w-24 rounded-full font-bold hover:opacity-80 focus:outline-none'>
+                Sign Up
+            </button>
+        </div>
+    );
+}
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,14 @@
+import SignupBtn from './components/SignupBtn';
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-    </h1>
+      <div>
+        <div className='bg-orange-500 text-white w-full h-14 flex justify-between'>
+            <div className='text-xl font-bold ml-4 pt-2.5'>OneFeed</div>
+            <div className='mr-4'>
+                <SignupBtn />
+            </div>            
+        </div>        
+      </div>
   );
 }
