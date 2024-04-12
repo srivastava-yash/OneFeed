4dd3f45b1003765e6955eb910626373e4296b92a
srivastava-yash <yash.184040@gmail.com>
2024-04-11 22:45:10 -0400

    frontend package.json update

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,14 @@
 import SignupBtn from './components/SignupBtn';
 
 export default function Home() {
-  return (
-      <div>
-        <div className='bg-orange-500 text-white w-full h-14 flex justify-between'>
-            <div className='text-xl font-bold ml-4 pt-2.5'>OneFeed</div>
-            <div className='mr-4'>
-                <SignupBtn />
-            </div>            
-        </div>        
-      </div>
-  );
+    return (
+        <div>
+            <div className='bg-orange-500 text-white w-full h-14 flex justify-between'>
+                <div className='text-xl font-bold ml-4 pt-2.5'>OneFeed</div>
+                <div className='mr-4'>
+                    <SignupBtn />
+                </div>
+            </div>
+        </div>
+    );
 }
