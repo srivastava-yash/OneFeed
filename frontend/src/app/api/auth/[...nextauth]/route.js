2bce2bf8735b584eadf7dd2c71daf1f7ce5d5ae1
srivastava-yash <yash.184040@gmail.com>
2024-04-11 22:43:45 -0400

    next app setup

---
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+
+const handler = NextAuth({
+    providers : [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_AUTH_ID,
+            clientSecret: process.env.GOOGLE_AUTH_SECRET
+        })
+    ],
+    callbacks: {
+        async session({ session }) {
+            return session;
+        },
+
+        async signIn({ profile }) {
+            console.log(profile);
+            // can get profile.email profile.picture profile.name
+        },
+
+
+    }
+
+})
+
+export { handler as GET, handler as POST };
